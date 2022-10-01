d33d9618435714bf4e78601debf042d60ee6da28
David "Dregel" Bengtsson <david@witchcult.se>
2022-10-01 21:48:30 +0100

    Lab bin

--- src/bin/lab.rs
+++ src/bin/lab.rs
@@ -1,9 +1,22 @@
 extern crate seeed_studio_relay_board;
+extern crate gameprng;
 
+use std::{thread, time};
 use seeed_studio_relay_board::RelayBoard;
+use gameprng::prng::Prng;
+use gameprng::xoroshiro128plus::XoRoShiRo128Plus;
 
 fn main() {
     let mut rb = RelayBoard::new(0x20);
+    let d = time::Duration::new(0, 100000000);
+    let mut prng = Prng::<XoRoShiRo128Plus>::new(666);
 
-    rb.relay_on(1);
+    rb.relay_all_off();
+
+    for _ in 0..500 {
+        thread::sleep(d);
+        rb.relay_toggle(prng.range(1, 4));
+    }
+
+    rb.relay_all_off();
 }
